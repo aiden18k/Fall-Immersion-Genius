2413ed9e8a68463d7439e456dd0623a1281ae158
aiden18k <39xtylerr@gmail.com>
2024-10-22 17:51:13 -0400

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,12 @@
+const htmlBody = document.body;
 function darkMode() {
-// Your code here.
+if(htmlBody.style.backgroundColor === 'white'){
+   htmlBody.style.backgroundColor = 'black'; 
+   htmlBody.style.color = 'white';
+  }
+else{
+  htmlBody.style.backgroundColor = 'white';
+  htmlBody.style.color = 'black';
+  }
 }
+  
