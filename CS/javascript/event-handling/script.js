42fe985a052e9ff5e7bb53ac1bb6da30dfee62cf
aiden18k <39xtylerr@gmail.com>
2024-12-15 20:12:51 -0500

    Update script.js

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,18 @@
-function eventHandling() {
-    // Your code here.
-}
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Event Handling</title>
+    </head>
+    <body>
+        <h1>Event Handling</h1>
+        <button id="myButton">Click me</button>
+        <script src="script.js"></script>
+    </body> 
+</html>
+    document.getElementById('').addEventListener('click', function()  {
+    imageHolder = document.createElement('img');
+    imageHolder.src = "https://miro.medium.com/v2/resize:fit:1400/0*2nmrdjWd6JwULegK";
+    document.body.appendChild(imageHolder);
+});
